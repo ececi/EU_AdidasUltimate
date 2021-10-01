ca17df6c0160ce2f9d014f6ed481ac4b30190610
ececi <huseyinkiceci@gmail.com>
2021-10-01 16:57:10 +0300

    abstracted almost all string literal definitions from the test class. now Adidas class is more useful. Test class is more independent and easily editable in case of locator changes.

--- src/test/java/modals/Adidas.java
+++ src/test/java/modals/Adidas.java
@@ -2,11 +2,12 @@ package modals;
 
 import enums.EProductCategory;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class Adidas {
-
-    public static final String addToChartLinkText = "Add to cart";
-    public static final By byCart = new By.ByLinkText("Cart");
+    static WebDriver driver;
+    private static final String addToChartLinkText = "Add to cart";
 
     private String product;
     private final String linkText;
@@ -19,6 +20,10 @@ public class Adidas {
         this.linkText = product;
     }
 
+    public static void setupDriver(WebDriver driver){
+        Adidas.driver = driver;
+    }
+
     public EProductCategory getProductCategory(){return productCategory;}
 
     public String getLinkText(){return linkText;}
@@ -27,9 +32,79 @@ public class Adidas {
 
     public void setAmount(double amount){this.amount = amount;}
 
+    public WebElement getMenuCategory(){
+        return driver.findElement(By.linkText(getProductCategory().getLinkText()));
+    }
+
+    public WebElement getProductLink(){
+        return driver.findElement(By.linkText(getLinkText()));
+    }
+
+    public WebElement getButtonAddToChart(){
+        return driver.findElement(By.linkText(Adidas.addToChartLinkText));
+    }
+
     public static Adidas getInstance(EProductCategory productCategory, String product) {
         return new Adidas(productCategory, product);
     }
 
+    public static class Elements{
+        public static WebElement getButtonCart(){
+            return driver.findElement(By.linkText("Cart"));
+        }
 
+        public static WebElement getButtonPlaceOrder(){
+            return driver.findElement(By.xpath("//button[.='Place Order']"));
+        }
+
+        /**
+         * @param productIndex zero based index
+         * @return
+         */
+        public static WebElement getButtonRemoveProductByProductIndex(int productIndex){
+            return driver.findElement(By.cssSelector("#tbodyid tr:nth-of-type(" + (productIndex + 1) + ") td:nth-of-type(4) a"));
+        }
+
+        public static class PurchaseForm{
+            public static WebElement getTextBoxName(){
+                return driver.findElement(By.id("name"));
+            }
+            public static WebElement getTextBoxCountry(){
+                return driver.findElement((By.id("country")));
+            }
+            public static WebElement getTextBoxCity(){
+                return driver.findElement(By.id("city"));
+            }
+            public static WebElement getTextBoxCreditCard(){
+                return driver.findElement(By.id("card"));
+            }
+            public static WebElement getTextBoxMonth(){
+                return driver.findElement(By.id("month"));
+            }
+            public static WebElement getTextBoxYear(){
+                return driver.findElement(By.id("year"));
+            }
+            public static WebElement getButtonPurchase(){
+                return driver.findElement(By.xpath("//button[.='Purchase']"));
+            }
+            public static WebElement getButtonOK(){
+                return driver.findElement(By.xpath("//button[.='OK']"));
+            }
+            private static String[] getPurchaseResultsAsParts(){
+                WebElement pPurchaseResult = driver.findElement(By.cssSelector(".lead.text-muted"));
+                String[] resultParts = pPurchaseResult.getAttribute("innerHTML").split("<br>");
+                return resultParts;
+            }
+            public static int getResultID(){
+                int resultID = Integer.parseInt(getPurchaseResultsAsParts()[0].split(" ")[1]);
+                return resultID;
+            }
+            public static double getResultAmount(){
+                double resultAmount = Double.parseDouble(getPurchaseResultsAsParts()[1].split(" ")[1]);
+                return resultAmount;
+            }
+        }
+
+
+    }
 }
--- src/test/java/tests/AdidasTest.java
+++ src/test/java/tests/AdidasTest.java
@@ -18,11 +18,13 @@ import org.testng.annotations.Test;
 
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import static modals.Adidas.Elements;
+
 public class AdidasTest {
-    WebDriver driver;
+    private WebDriver driver;
+    private ArrayList<Adidas> productsInChart;
 
     @BeforeMethod
     void setupBeforeMethod(){
@@ -30,19 +32,23 @@ public class AdidasTest {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
+        Adidas.setupDriver(driver);
     }
 
     @AfterMethod
     void tearDownMethod() throws InterruptedException {
-        Thread.sleep(5000);
+        Thread.sleep(2200);
         driver.quit();
     }
 
     private void addToChart(Adidas product){
+        if (productsInChart == null)
+            productsInChart = new ArrayList<>();
+
         driver.get("https://www.demoblaze.com/index.html");
-        driver.findElement(By.linkText(product.getProductCategory().getLinkText())).click();
-        driver.findElement(By.linkText(product.getLinkText())).click();
-        driver.findElement(By.linkText(Adidas.addToChartLinkText)).click();
+        product.getMenuCategory().click();
+        product.getProductLink().click();
+        product.getButtonAddToChart().click();
 
         new WebDriverWait(driver, 4)
                 .ignoring(NoAlertPresentException.class)
@@ -51,6 +57,15 @@ public class AdidasTest {
         driver.switchTo().alert().accept();
 
         product.setAmount(getCurrentProductAmount());
+        productsInChart.add(product);
+    }
+
+    /**
+     * @param productIndexToRemove zero based index
+     */
+    private void removeFromChart(int productIndexToRemove){
+        Elements.getButtonRemoveProductByProductIndex(productIndexToRemove).click();
+        productsInChart.remove(productIndexToRemove);
     }
 
     private Double getCurrentProductAmount(){
@@ -59,54 +74,38 @@ public class AdidasTest {
 
     @Test
     void test() throws InterruptedException {
+        addToChart(Adidas.getInstance(EProductCategory.category_Laptop, "Sony vaio i5"));
+        addToChart(Adidas.getInstance(EProductCategory.category_Laptop, "Dell i7 8gb"));
 
-        ArrayList<Adidas> productsInChart = new ArrayList<>();
-
-        productsInChart.add(Adidas.getInstance(EProductCategory.category_Laptop, "Sony vaio i5"));
-        addToChart(productsInChart.get(productsInChart.size() - 1));
-
-        productsInChart.add(Adidas.getInstance(EProductCategory.category_Laptop, "Dell i7 8gb"));
-        addToChart(productsInChart.get(productsInChart.size() - 1));
-
-        driver.findElement(Adidas.byCart).click();
+        Adidas.Elements.getButtonCart().click();
 
         String urlCart = driver.getCurrentUrl();
 
-        int productIndexToRemove = 1;//zero based index
-        driver.findElement(By.cssSelector("#tbodyid tr:nth-of-type(" + (productIndexToRemove + 1) + ") td:nth-of-type(4) a")).click();
-        productsInChart.remove(productIndexToRemove);
+        removeFromChart(1);
 
         //This sleep's target is to give a time to web page for removing the product
-        Thread.sleep(2000);
+        Thread.sleep(3000);
 
         driver.get(urlCart);
 
-        driver.findElement(By.xpath("//button[.='Place Order']")).click();
+        Elements.getButtonPlaceOrder().click();
 
         Faker faker = new Faker();
 
-        WebElement txtName = driver.findElement(By.id("name"));
-
-        txtName.sendKeys(faker.name().fullName());
-        driver.findElement((By.id("country"))).sendKeys(faker.country().name());
-        driver.findElement(By.id("city")).sendKeys(faker.address().cityName());
-        driver.findElement(By.id("card")).sendKeys(faker.finance().creditCard());
-        driver.findElement(By.id("month")).sendKeys(String.valueOf(faker.number().numberBetween(1, 12)));
+        Elements.PurchaseForm.getTextBoxName().sendKeys(faker.name().fullName());
+        Elements.PurchaseForm.getTextBoxCountry().sendKeys(faker.country().name());
+        Elements.PurchaseForm.getTextBoxCity().sendKeys(faker.address().cityName());
+        Elements.PurchaseForm.getTextBoxCreditCard().sendKeys(faker.finance().creditCard());
+        Elements.PurchaseForm.getTextBoxMonth().sendKeys(String.valueOf(faker.number().numberBetween(1, 12)));
 
         int currentYear = Calendar.getInstance().get(Calendar.YEAR);
 
-        driver.findElement(By.id("year")).sendKeys(String.valueOf(faker.number().numberBetween(currentYear, currentYear + 10)));
+        Elements.PurchaseForm.getTextBoxYear().sendKeys(String.valueOf(faker.number().numberBetween(currentYear, currentYear + 10)));
 
-        driver.findElement(By.xpath("//button[.='Purchase']")).click();
+        Elements.PurchaseForm.getButtonPurchase().click();
 
-        WebElement pPurchaseResult = driver.findElement(By.cssSelector(".lead.text-muted"));
+        Assert.assertEquals(Elements.PurchaseForm.getResultAmount(), productsInChart.stream().mapToDouble(Adidas::getAmount).sum());
 
-        String[] resultParts = pPurchaseResult.getAttribute("innerHTML").split("<br>");
-        int resultID = Integer.parseInt(resultParts[0].split(" ")[1]);
-        double resultAmount = Double.parseDouble(resultParts[1].split(" ")[1]);
-
-        Assert.assertEquals(resultAmount, productsInChart.stream().mapToDouble(Adidas::getAmount).sum());
-
-        driver.findElement(By.xpath("//button[.='OK']")).click();
+        Elements.PurchaseForm.getButtonOK().click();
     }
 }
